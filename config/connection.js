b6ab2229acc6a8f2d197e9da4a2e9cba1a7c1091
Ben G <bensgar92@gmail.com>
2020-09-20 17:42:29 -0600

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+//setting up jawsDB
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burger_db'
+  });
+};
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
